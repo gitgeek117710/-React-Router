dd49a84e3f4ec1bf05aeb6843ba6d18a7bb954ed
gitgeek117710 <gitgeek117710@gmail.com>
2025-07-17 17:52:01 +0800

    Prepare for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const repoName = '-React-Router'; // e.g., 'sublocations-react'
+
 export default defineConfig({
   plugins: [react()],
+  base: `/${repoName}/`, // 👈 Important for GitHub Pages
 })
