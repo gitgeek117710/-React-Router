66e1f87c169b411cdb7d9c8af3702482e827823f
gitgeek117710 <gitgeek117710@gmail.com>
2025-07-17 18:03:51 +0800

    corrections

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-const repoName = '-React-Router'; // e.g., 'sublocations-react'
+const repoName = '/-React-Router/'; // e.g., 'sublocations-react'
 
 export default defineConfig({
   plugins: [react()],
